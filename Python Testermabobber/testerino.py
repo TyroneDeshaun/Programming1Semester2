dfa839f226ff5555fab1b43b01c80dcb700e693e
Alec Tran <9514555@646-0303-VPC34.graniteschools.org>
2016-02-16 14:00:41 -0700

    i dont even know
    kekekekekekkekeke

--- Python Testermabobber/testerino.py
+++ Python Testermabobber/testerino.py
@@ -1,4 +1,6 @@
 import random
+
+isRunning = str(input("Type if Yes if you want this program to work.(Caps matters) "))
 oPQuestions = ("What is the meaning of life? ",
                "Where did we come from? ",
                "Who is the most important person on earth? ",
@@ -9,9 +11,12 @@ oPQuestions = ("What is the meaning of life? ",
                "What do you think you symbolize? ",
                "Would you sacrifice 100 close people to you for the world? ",
                "Would you sacrifice the world for 100 people close to you? ")
-               
-x = str(input(random.choice(oPQuestions)))
 
-my_file = open("kappa.txt","w")
-my_file.write((x))
-my_file.close()
+while((isRunning) == "Yes"):
+    x = str(input(random.choice(oPQuestions)))
+    my_file = open("kappa.txt","a")
+    my_file.write((x) + "\n")
+    my_file.close()
+    isRunning = str(input("Type Stop if you want this program to stop.(Caps matters. "))
+else:
+    print("K cya")
